7090f02bb2c38be5f46b2336665d330babb90f26
mateoc11 <mateocifuentes612@gmail.com>
2023-06-15 10:41:54 -0500

    Creation of the webhook using an Azure Logic Apps endpoint

---
+++ Products/3_Webhook_creation.py
@@ -0,0 +1,18 @@
+import shopify
+import Keys
+
+
+##setup the connections to shopify
+shopify.Session.setup(api_key=Keys.api_key, secret=Keys.secret)
+
+session = shopify.Session(Keys.shop_url,'2023-04',Keys.access_token)
+
+##activate the session
+shopify.ShopifyResource.activate_session(session)
+
+
+shopify.Webhook.create({"topic":"products/update",
+                       "address": "https://azla-webhook-mcgstore.azurewebsites.net:443/api/logs-products-updates/triggers/When_a_HTTP_request_is_received/invoke?api-version=2022-05-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=iqIfnLLVzC0BESfqYEWP0Vd57BtyxE1YWxziec5Qrgg",
+                       "format":"json"})
+
+shopify.ShopifyResource.clear_session()
