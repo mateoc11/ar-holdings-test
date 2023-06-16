e68349a98de240b0fe57eba73910d8d0d603bec2
mateoc11 <mateocifuentes612@gmail.com>
2023-06-15 20:27:47 -0500

    Added the color picker elements

--- UI/ar_holdings/views.py
+++ UI/ar_holdings/views.py
@@ -17,11 +17,24 @@ def findProduct(request):
     if request.method == 'POST':  
         sku = request.POST['SKU']
         products = Products.objects.filter(SKU__contains=f'{sku}').order_by('-ID')
+        colors = ""
+
         for product in products:
             images = product.Images
+            if product.Type == 'variable':
+                colors = product.Attribute_2_value.split('|')
             break
+        
+        color_stylings= []
+        
+        for color in colors:
+            color_stylings.append(f'''style="background-color: {color.lower()} !important;
+                                      border-radius:55px;
+                                      height: 35px;
+                                      margin-left:10px;"''')
+
         return render(
         request,
         'products/product.html',
-        {'product': products, 'images': images.split(",")}
+        {'product': products, 'images': images.split(","), 'colors':color_stylings}
         )
